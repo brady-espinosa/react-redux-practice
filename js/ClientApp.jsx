ece7ae8259489150d5de7f733b0f8bdf322bda9f
Brady Espinosa <brady@bradys-mbp.attlocal.net>
2017-09-27 16:25:49 -0700

    practice with no jsx, added prettier for formating

---
+++ js/ClientApp.jsx
@@ -0,0 +1,22 @@
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce(
+    "div",
+    null,
+    ce("div", { style: { color: props.color } }, props.title)
+  );
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    "div",
+    null,
+    ce(MyTitle, { title: "Game for Thrones", color: "red" }),
+    ce(MyTitle, { title: "Stranger Things", color: "peru" }),
+    ce(MyTitle, { title: "Rick and Morty" }),
+    ce(MyTitle, { title: "Poop Mcgee" })
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById("app"));
