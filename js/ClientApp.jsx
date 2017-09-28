04efd0e9de1c24cdadad95847e7d0c45dbff58c9
Brady Espinosa <brady@Bradys-MacBook-Pro.local>
2017-09-28 14:06:11 -0700

    webpack configured

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,10 +1,11 @@
-const ce = React.createElement;
+import React from "react";
+import { render } from "react-dom";
 
 const MyTitle = function(props) {
   return ce(
     "div",
     null,
-    ce("div", { style: { color: props.color } }, props.title)
+    ce("div", { style: { color: props.color } }, props.title),
   );
 };
 
@@ -12,10 +13,11 @@ const MyFirstComponent = function() {
   return ce(
     "div",
     null,
+
     ce(MyTitle, { title: "Game for Thrones", color: "red" }),
     ce(MyTitle, { title: "Stranger Things", color: "peru" }),
     ce(MyTitle, { title: "Rick and Morty" }),
-    ce(MyTitle, { title: "Poop Mcgee" })
+    ce(MyTitle, { title: "Poop Mcgee" }),
   );
 };
 
