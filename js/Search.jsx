8f7b95cc20448fbbde1eee9f1a83775e432eff44
Brady Espinosa <brady@bradys-mbp.attlocal.net>
2017-10-03 12:27:16 -0700

    managing state, adding properties to es6 classes

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: '',
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
 
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>Sick Videos</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
